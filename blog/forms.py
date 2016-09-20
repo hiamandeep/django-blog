2ad3a7cfe62e778591e8c523878b87944a294845
Aman Deep <amandeep1024@gmail.com>
2016-09-20 22:01:16 +0530

    Added Post Edit, Tags, Pagination, Recent Posts and improved UI

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
-
+	# title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-style',}))
 	class Meta:
 		model = Post
-		fields = ('title','text',)
+		fields = ('title','text','tags')
